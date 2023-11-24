486d161930c81d74bb89011351b638027d800bc6
Max van Essen <max.vanessen@icloud.com>
2023-11-24 16:01:03 +0100

    Update Day0 example code

--- src/y2023/Day0.kt
+++ src/y2023/Day0.kt
@@ -4,20 +4,19 @@ import utils.get
 
 // Template
 fun main() {
-    println(get(day = 1, year = 2022))
-//    fun part1(input: List<String>): Int {
-//        return input.size
-//    }
-//
-//    fun part2(input: List<String>): Int {
-//        return input.size
-//    }
-//
-//    // test if implementation meets criteria from the description, like:
-//    val testInput = get(day = 1, suffix = "_test")
-//    check(part1(testInput) == 1)
-//
-//    val input = get(day = 1)
-//    println(part1(input))
-//    println(part2(input))
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = get(day = 1, suffix = "_test")
+    check(part1(testInput) == 1)
+
+    val input = get(day = 1)
+    println(part1(input))
+    println(part2(input))
 }
